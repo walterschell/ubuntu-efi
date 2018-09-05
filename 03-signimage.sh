15c2505dddeb117c082b01045e1c588b9e8c594e
Walter Schell <walterschell@users.noreply.github.com>
2018-09-05 16:17:19 -0400

    updates kernel sigs as well

--- 03-signimage.sh
+++ 03-signimage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ls /boot/vmlinuz* | xargs -I{} -n1 sudo sbsign --key test-key.rsa --cert test-cert.pem --output {} {}
 sudo sbsign --key test-key.rsa --cert test-cert.pem \
         --output grubx64.efi /boot/efi/efi/ubuntu/grubx64.efi
 sudo cp /boot/efi/efi/ubuntu/grubx64.efi{,.bak}
